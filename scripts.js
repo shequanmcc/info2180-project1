5264370f9d77861396e744653da8ebc63886b9b0
shequanmcc <shequanmccalla@gmail.com>
2025-11-22 22:16:18 -0500

    Completed landing page layout, styling and newsletter JS

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
-/* Add your JavaScript to this file */
+window.onload = function () {
+    const form = document.querySelector("form");
+    const emailInput = document.querySelector("#email");
+    const messageDiv = document.querySelector(".message");
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault(); 
+        const email = emailInput.value.trim();
+
+        if (email === "") {
+            messageDiv.textContent = "Please enter a valid email address.";
+            messageDiv.style.color = "white";
+        } else {
+            messageDiv.innerHTML =
+                `Thank you! Your email address <strong>${email}</strong> has been added to our mailing list!`;
+            messageDiv.style.color = "white";
+
+           
+            emailInput.value = "";
+        }
+    });
+};
